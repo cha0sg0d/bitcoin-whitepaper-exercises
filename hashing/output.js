12d80a8b1ffb2e31435fb4e30cef18a02d6e67bd
cha0sg0d <tony.goss@protonmail.com>
2021-09-01 22:16:13 -0400

    output to compare poem1 has diff hash

---
+++ hashing/output.js
@@ -0,0 +1,51 @@
+[
+  { index: 0, hash: '000000', data: '' },
+  {
+    index: 1,
+    prevHash: '000000',
+    data: 'The power of a gun can kill',
+    hash: '2dc0350c921b0a360bda78793c947746bc7be88732711eb58ca8320a623297c3'
+  },
+  {
+    index: 2,
+    prevHash: '2dc0350c921b0a360bda78793c947746bc7be88732711eb58ca8320a623297c3',
+    data: 'and the power of fire can burn',
+    hash: '93240ce6f11d48b9c218628bb157ffe4d3cddea3dfd84c7b54d5b2947a53c06f'
+  },
+  {
+    index: 3,
+    prevHash: '93240ce6f11d48b9c218628bb157ffe4d3cddea3dfd84c7b54d5b2947a53c06f',
+    data: 'the power of wind can chill',
+    hash: '8474de3c7b664bcff0ddf8df8da7563d00a33a2d55726e2a7d0178609dae41c7'
+  },
+  {
+    index: 4,
+    prevHash: '8474de3c7b664bcff0ddf8df8da7563d00a33a2d55726e2a7d0178609dae41c7',
+    data: 'and the power of a mind can learn',
+    hash: 'cf5668efb87aacb97eabe7c302d5cb5e8c167b2e189cc6086a87cb4bef9c4394'
+  },
+  {
+    index: 5,
+    prevHash: 'cf5668efb87aacb97eabe7c302d5cb5e8c167b2e189cc6086a87cb4bef9c4394',
+    data: 'the power of anger can rage',
+    hash: '7c2489f429a09886305e59fc8392344d19619da3b38a544e9ecc6c70e2a46402'
+  },
+  {
+    index: 6,
+    prevHash: '7c2489f429a09886305e59fc8392344d19619da3b38a544e9ecc6c70e2a46402',
+    data: 'inside until it tears u apart',
+    hash: 'f5a545b9b086c7833b5969bcffa7d5df939ff95a6a3a01b5d4ddf831fa05c9ce'
+  },
+  {
+    index: 7,
+    prevHash: 'f5a545b9b086c7833b5969bcffa7d5df939ff95a6a3a01b5d4ddf831fa05c9ce',
+    data: 'but the power of a smile',
+    hash: 'a22127469e0d7bf7fe26fb8545fdb207d2beb9ed90805cad79a7173cff243396'
+  },
+  {
+    index: 8,
+    prevHash: 'a22127469e0d7bf7fe26fb8545fdb207d2beb9ed90805cad79a7173cff243396',
+    data: 'especially yours can heal a frozen heart',
+    hash: '416018fe91caa28d475e3bd93735bb53b2f486ea9f5e1faa6c794524465866fb'
+  }
+]
---
+++ hashing/output1.js
@@ -0,0 +1,51 @@
+[
+  { index: 0, hash: '000000', data: '' },
+  {
+    index: 1,
+    prevHash: '000000',
+    data: 'The power of a gun can kilt',
+    hash: '4bb46b57919a6aa90328447c501a9b35ae9f3703f8e1eca81a9e7d3509fbf459'
+  },
+  {
+    index: 2,
+    prevHash: '4bb46b57919a6aa90328447c501a9b35ae9f3703f8e1eca81a9e7d3509fbf459',
+    data: 'and the power of fire can burn',
+    hash: '43f3847e63cbe3c973daaed28f90c5761e710127e7b5028fec8bdb63c67d71a6'
+  },
+  {
+    index: 3,
+    prevHash: '43f3847e63cbe3c973daaed28f90c5761e710127e7b5028fec8bdb63c67d71a6',
+    data: 'the power of wind can chill',
+    hash: '0c1ca0bb11bd8cac2f8dfafbc97285eb878ec3779f59149b4eec146f58a3da95'
+  },
+  {
+    index: 4,
+    prevHash: '0c1ca0bb11bd8cac2f8dfafbc97285eb878ec3779f59149b4eec146f58a3da95',
+    data: 'and the power of a mind can learn',
+    hash: '573fb3c5b3946635db851dc3928926f77f00538b2ac7a5fc0f7daf4280ed73e5'
+  },
+  {
+    index: 5,
+    prevHash: '573fb3c5b3946635db851dc3928926f77f00538b2ac7a5fc0f7daf4280ed73e5',
+    data: 'the power of anger can rage',
+    hash: '4d3219c55ab6ad56ba1dd5e542fd0f519378ea6514b99ad5c7576c2aaf2a115d'
+  },
+  {
+    index: 6,
+    prevHash: '4d3219c55ab6ad56ba1dd5e542fd0f519378ea6514b99ad5c7576c2aaf2a115d',
+    data: 'inside until it tears u apart',
+    hash: '4e28053d542c6a22cfe5840e9037ee226cc78aba324facefe38d1216c45fd39a'
+  },
+  {
+    index: 7,
+    prevHash: '4e28053d542c6a22cfe5840e9037ee226cc78aba324facefe38d1216c45fd39a',
+    data: 'but the power of a smile',
+    hash: '3b8368b403ece47c430862d893a99fae1ca8621f9f0d886c39527e05a364acca'
+  },
+  {
+    index: 8,
+    prevHash: '3b8368b403ece47c430862d893a99fae1ca8621f9f0d886c39527e05a364acca',
+    data: 'especially yours can heal a frozen heart',
+    hash: 'c282e2fa7ea274142ccca82b87d9d69cdcd26be0daa682bc289ed4b6c8729904'
+  }
+]
