e31857215469e9cbe73f4c92aaef822449fa3342
cha0sg0d <tony.goss@protonmail.com>
2021-09-03 18:44:59 -0400

    working pow

--- pow/pow.js
+++ pow/pow.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var crypto = require("crypto");
+const { connect } = require("http2");
 
 // The Power of a Smile
 // by Tupac Shakur
@@ -17,6 +18,9 @@ var poem = [
 
 var difficulty = 10;
 
+const BITS_IN_HEX = 4;
+const HEX_BASE = 16;
+
 var Blockchain = {
 	blocks: [],
 };
@@ -32,11 +36,14 @@ Blockchain.blocks.push({
 for (let line of poem) {
 	let bl = createBlock(line);
 	Blockchain.blocks.push(bl);
-	console.log(`Hash (Difficulty: ${difficulty}): ${bl.hash}`);
+	console.log(`Hash (Difficulty: ${difficulty}): ${bl.hash} for ${bl.nonce} reps`);
 
 	difficulty++;
 }
 
+// console.dir(Blockchain.blocks);
+
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 // **********************************
 
@@ -47,19 +54,44 @@ function createBlock(data) {
 		data,
 		timestamp: Date.now(),
 	};
+  
+  bl.nonce = 0;
 
-	bl.hash = blockHash(bl);
-
+  bl.hash = blockHash(bl)
+  while (!hashIsLowEnough(bl.hash)) {
+    bl.nonce = bl.nonce + 1;
+    bl.hash = blockHash(bl);
+  }
 	return bl;
 }
 
+// copy from ../hashing
 function blockHash(bl) {
-	// TODO
+  let clone = {...bl}
+  delete clone.hash; // remove hash property. the hash itself is not needed
+	let hash = crypto.createHash("sha256")
+    .update(JSON.stringify(clone))
+    .digest("hex"); 
+  // console.log(`new hash`, hash)
+  return hash;
 }
 
 function hashIsLowEnough(hash) {
-	// TODO
+  // console.log(`hash is ${hash}`);
+  const numHexChars = Math.floor(difficulty / BITS_IN_HEX) + 1;
+  const firstXChars = hash.slice(0, numHexChars);
+  // console.log(`first ${numHexChars} hex chars of hash ${firstXChars}`);
+  // number representing target w corret number of zeros
+  const binaryTarget = "0b" + ("1".padStart(difficulty, "0"))
+  // console.log(`binary target ${binaryTarget}`);
+  const numTarget = Number(binaryTarget);
+  // number representing first x hex chars of hash, converted to number.
+  const hashNum = parseInt(firstXChars, HEX_BASE)
+  // console.log(`hashNum is ${hashNum} with ${numHexChars} chars`);
+  // console.log(`is hashnum ${hashNum} < target ${numTarget}? ${hashNum < numTarget}`)
+  return hashNum < numTarget;
 }
+
 
 function verifyBlock(bl) {
 	if (bl.data == null) return false;
@@ -75,7 +107,9 @@ function verifyBlock(bl) {
 		)) {
 			return false;
 		}
-		if (bl.hash !== blockHash(bl)) return false;
+		if (bl.hash !== blockHash(bl)) {
+      return false
+    };
 	}
 
 	return true;
@@ -85,8 +119,10 @@ function verifyChain(chain) {
 	var prevHash;
 	for (let bl of chain.blocks) {
 		if (prevHash && bl.prevHash !== prevHash) return false;
+
 		if (!verifyBlock(bl)) return false;
-		prevHash = bl.hash;
+
+	  prevHash = bl.hash;
 	}
 
 	return true;
